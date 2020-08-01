6f50742d88026b42bb3866c541a99c82b5f2ab35
alk3pInjection <webmaster@raspii.tech>
2020-08-01 19:21:16 +0800

    airKernel: Bring-up
    * Import ak3 & our build scripts.
    
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export ARCH=arm64
+export SUBARCH=arm64
+export CROSS_COMPILE=../build-tools/arm64-gcc/bin/aarch64-elf-
+export CROSS_COMPILE_ARM32=../build-tools/arm32-gcc/bin/arm-eabi-
+export KBUILD_BUILD_USER=misaka
+export KBUILD_BUILD_HOST=tp-workstation
+export KJOBS="$((`grep -c '^processor' /proc/cpuinfo` * 2))"
+VERSION="$(cat defconfig | grep "CONFIG_LOCALVERSION\=" | sed -r 's/.*"(.+)".*/\1/' | sed 's/^.//' | sed 's/^..........//')"
+
+cp defconfig .config
+make -j${KJOBS} || exit 1
+
+if [ -e arch/arm64/boot/Image.gz ] ; then
+	echo
+	echo "Building Kernel Package"
+	echo
+	mkdir kernelzip
+	mkdir kernelzip/source
+	cp -rp ./anykernel/* kernelzip/
+	cp arch/arm64/boot/Image.gz kernelzip/source/
+	find arch/arm64/boot/dts -name '*.dtb' -exec cat {} + > kernelzip/source/dtb
+	cd kernelzip
+	7z a -mx9 airKernel-$VERSION-tmp.zip *
+	zipalign -v 4 airKernel-$VERSION-tmp.zip ../airKernel-$VERSION.zip
+	rm airKernel-$VERSION-tmp.zip
+	cd ..
+	ls -al airKernel-$VERSION.zip
+fi
